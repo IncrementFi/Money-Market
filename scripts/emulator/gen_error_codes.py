2ab19035e8ed5bbec2350aa64d595e3f70fb0811
swordc <swordc@126.com>
2021-12-09 04:28:58 +0800

    update error code identify

--- scripts/emulator/gen_error_codes.py
+++ scripts/emulator/gen_error_codes.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import json
-
+import re
 
 ErrorIdentifierStart = False
 ErrorIndex = 0
@@ -11,12 +11,15 @@ ErrorJson = {}
 with open('./cadence/contracts/Error.cdc', 'r') as f:
     for line in f:
         line = line.strip()
-        if line.startswith('pub enum Error: UInt8'): ErrorIdentifierStart = True
+        if line.startswith('pub enum '): ErrorIdentifierStart = True
         if ErrorIdentifierStart and line.startswith('}'): ErrorIdentifierStart = False
         if ErrorIdentifierStart == False: continue
 
-        if line.startswith('pub case '):
-            msg = line[9:]
+        if line.find('pub case ')>=0:
+            msgReg = re.compile(r'pub case (.+?)\b')
+            res = msgReg.search(line)
+            
+            msg = res[1]
             ErrorJson[ErrorIndex] = {}
             ErrorJson[ErrorIndex]['eng'] = msg
             ErrorJson[ErrorIndex]['chn'] = msg+'的喵'
