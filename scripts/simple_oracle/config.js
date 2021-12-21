309bb9f1ec046aa56aeb5bbb8fb8f7aff4451c06
swordc <swordc@126.com>
2021-12-21 20:02:31 +0800

    update oracle config on blt

--- scripts/simple_oracle/config.js
+++ scripts/simple_oracle/config.js
@@ -8,6 +8,7 @@ module.exports = {
   updaterAccount: deployed[network].updaterAccount,
   updaterKeyIndex: deployed[network].updaterKeyIndex,
   feeds: deployed[network].feeds,
-  coingeckoMegaFeeds: 'https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum,flow,tether',
+  //coingeckoMegaFeeds: 'https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum,flow,tether',
+  coingeckoMegaFeeds: 'https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=blocto-token,flow,tether',
   heartbeat: 60 * 1000,  // 60 seconds
 };
